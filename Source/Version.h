41a44d0765463bce34808c201e39ad2595f400b9
Brian Green <briangreenery@gmail.com>
2014-07-07 13:37:22 -0700

    Bump version to 9.1.0 to avoid confusion

--- Source/Version.h
+++ Source/Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_h
 #define Version_h
 
-#define BFARCHIVE_COMMA_SEPARATED_VERSION 3,0,0,0
-#define BFARCHIVE_VERSION_STRING "3.0.0"
+#define BFARCHIVE_COMMA_SEPARATED_VERSION 9,1,0,0
+#define BFARCHIVE_VERSION_STRING "9.1.0"
 
 #endif
