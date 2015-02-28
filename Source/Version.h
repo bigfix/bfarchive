12de877604d32a0cc2ca5619a7df489a6369fc7a
Brian Green <briangreenery@gmail.com>
2015-02-27 21:07:11 -0800

    9.3.1

--- Source/Version.h
+++ Source/Version.h
@@ -17,7 +17,7 @@
 #ifndef Version_h
 #define Version_h
 
-#define BFARCHIVE_COMMA_SEPARATED_VERSION 9,3,0,0
-#define BFARCHIVE_VERSION_STRING "9.3.0"
+#define BFARCHIVE_COMMA_SEPARATED_VERSION 9,3,1,0
+#define BFARCHIVE_VERSION_STRING "9.3.1"
 
 #endif
